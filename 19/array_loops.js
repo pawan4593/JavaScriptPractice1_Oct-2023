a9e88c6c8cc7a7970b8fddf92766cf162a708061
pawan4593 <mr.pawan_patil@yahoo.com>
2023-10-16 21:22:15 +0530

    for-in for-of forEach Arrays.from() etc

---
+++ 19/array_loops.js
@@ -0,0 +1,26 @@
+let arr = [23,54,234,54,57,68,346,568,643,5] ;
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log(arr[i]) ;
+// }
+
+// arr.forEach(element =>{
+//     console.log(element*element) ;
+// });
+
+// // Array.from()
+// let name = "Harry" ;
+// let arrr = Array.from(name) ;
+// console.log(arrr) ;
+
+
+// for(i of arr){
+//     console.log(i) ;
+// }
+
+
+for(let i in arr){
+    console.log(arr[i] * arr[i]) ;
+}
+
+
