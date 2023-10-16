dfb010986023494cf460eb4ed98e649650404184
pawan4593 <mr.pawan_patil@yahoo.com>
2023-10-16 20:32:40 +0530

    delete concat splice slice sort reverse etc methods

---
+++ 18/array_methods2.js
@@ -0,0 +1,43 @@
+let marks = [ 61 , 2 , 8 , 6 , 24 , 63 , "zabsent" , 75 , null , 97 , false , undefined , 7]
+
+console.log(marks) ;
+
+// console.log(marks.length) ;
+
+// delete marks[0] ; 
+
+// console.log(marks) ;
+
+// console.log(marks.length) ;
+
+// console.log(marks[0]) ;
+
+
+// let marks2 = [23 , "as" , 89 , "new array"] ;
+
+// let newArray = marks.concat(marks2) ;
+
+// console.log(marks)
+// console.log(marks2)
+// console.log(newArray);
+
+// let compare = (a , b) =>{
+//     return a - b ;
+// }
+// marks.sort(compare) ;
+
+// console.log(marks) ;
+
+// marks.reverse() ;
+
+// console.log(marks) ;
+
+
+// let deletedElements = marks.splice(2 ,3 , 1021 , 1022 , 1023 , 1024) ;
+
+// console.log(marks) ;
+// console.log(deletedElements , typeof deletedElements) ;
+
+let sliced = marks.slice(2 , 90) ;
+
+console.log(sliced , typeof sliced) ;
