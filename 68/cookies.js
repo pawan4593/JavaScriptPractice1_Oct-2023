46ff19a4d264e2be6df171f1dacbb414c5984097
pawan4593 <mr.pawan_patil@yahoo.com>
2023-11-04 14:40:35 +0530

    Browser Cookies

---
+++ 68/cookies.js
@@ -0,0 +1,15 @@
+console.log(document.cookie);
+document.cookie = "name1 = 'john snow'" ;
+document.cookie = "name2 = 'john cena'" ;
+document.cookie = "name3 = 'john mcLaive'" ;
+
+let key = prompt("Enter thy key : ")
+let value = prompt("Enter thy value : ")
+// document.cookie = `${key} = ${value}` ; // Try entering key as -  ;;;
+document.cookie = `${encodeURIComponent(key)} = ${encodeURIComponent(value)}` ;
+
+// decodeURIComponent(key) / decodeURIComponent(value) function can be used to decode the encoded URIs 
+
+console.log(document.cookie);
+
+//Also there are options like path , expires etc.
