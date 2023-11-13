3cff128f3d9da44e13608879249eedbfc8610b3b
pawan4593 <mr.pawan_patil@yahoo.com>
2023-11-13 15:45:11 +0530

    92 > Exercise_Q1.js corrected for my solution

--- 92/Exercise_Q1.js
+++ 92/Exercise_Q1.js
@@ -8,38 +8,39 @@
 // } , 2000))()
 
 
-// Pawan :Not Working , Why ?
-// const a = async (text)=>{
-//     setTimeout((text)=>{
-//         console.log(text);
-//     },2000) ;
-// }
+// Pawan :Working
+const a = async (text)=>{
+    // setTimeout((text)=>{ //The text parameter is shadowing the text parameter of the outer async function, so it will always log undefined.
+    setTimeout(()=>{
+        console.log(text);
+    },2000) ;
+}
 
-// (
-//     async ()=>{ // Withoud this arrow function , Error : TypeError: a(...) is not a function
-//         await a("Hello") 
-//         await a("World") 
-//     }
-// )()
+(
+    async ()=>{ // Withoud this arrow function , Error : TypeError: a(...) is not a function
+        await a("Hello") 
+        await a("World") 
+    }
+)()
 
 
 
 //CWH : Working
-const a = (text)=>{
-    return new Promise((resolve , reject)=>{
-        setTimeout(()=>{
-            // console.log(text +  " ");
-            resolve(text+" ");
-        },2000) ;
-    }) ;
-}
+// const a = (text)=>{
+//     return new Promise((resolve , reject)=>{
+//         setTimeout(()=>{
+//             // console.log(text +  " ");
+//             resolve(text+" ");
+//         },2000) ;
+//     }) ;
+// }
 
-(
-    async ()=>{
-        let text = await a("Hello") ;
-        console.log(text);
-        text = await a("World") ;
-        console.log(text);
-    }
-)()
+// (
+//     async ()=>{
+//         let text = await a("Hello") ;
+//         console.log(text);
+//         text = await a("World") ;
+//         console.log(text);
+//     }
+// )()
 
